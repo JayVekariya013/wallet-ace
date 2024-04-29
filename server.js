ca3c356176a637a82e20a9f788f17c0f65be5114
Jay Vekariya <jayvekariya013@gmail.com>
2024-04-29 15:48:06 +0530

    update 2.0

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
-// Connect to MongoDB
-// mongoose.connect('mongodb://localhost:27017/expenses?connectTimeoutMS=5000&directConnection=true&serverSelectionTimeoutMS=5000', {
-//   useUnifiedTopology: true,
-// })
-// .then(() => console.log('Connected to MongoDB'))
-// .catch(err => console.error('Error connecting to MongoDB:', err));
-
 const dbConnect = require("./config/database");
 dbConnect();
 
@@ -30,7 +22,10 @@ const expenseSchema = new mongoose.Schema({
   paymentAccount: { type: String, required: true }
 });
 
+
 const Expense = mongoose.model('Expense', expenseSchema);
+const Users = require('./models/user');
+const Account = require('./models/cardForm')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,6 +35,7 @@ const signupRoute = require('./routes/signup');
 app.use('/api', signupRoute);
 
 const loginRoute = require('./routes/login');
+const user = require('./models/user');
 app.use('/api', loginRoute);
 
 // Middleware to parse JSON bodies
@@ -75,18 +71,112 @@ app.get('/api/expenses', (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     });
 });
+// Route to retrieve expenses data
+app.post('/api/signup', async (req, res) => {
+  console.log("req", req.body)
+  const { email, sPassword, sName } = req.body;
+  if (!email || !sPassword || !sName) {
 
-// Endpoint to fetch expenses data
-app.get('/api/getExpenses', async (req, res) => {
+    res.status(400).send({
+      code: 400,
+      message: "please enter proper values."
+    })
+  }
+  const createUserb = await Users.create({
+    username: sName,
+    email: email,
+    password: sPassword,
+  });
+  console.log('createUserb :>> ', createUserb);
+
+  res.status(200).send({
+    code: 200,
+    message: "Successfully register"
+  })
+
+});
+app.post('/api/login', async (req, res) => {
+  console.log("req", req.body)
+  const { email, sPassword } = req.body;
+  if (!email || !sPassword) {
+
+    res.status(400).send({
+      code: 400,
+      message: "please enter proper values."
+    })
+  }
+  const findUserAuth = await Users.findOne({
+    email: email,
+    password: sPassword
+  });
+  console.log("findUserAuth", findUserAuth)
+  if (findUserAuth && findUserAuth.data) {
+
+  }
+  console.log('findUserAuth :>> ', findUserAuth);
+
+  res.status(200).send({
+    code: 200,
+    message: "Successfully register"
+  })
+
+});
+
+app.post('/api/account', async (req, res) => {
+  // console.log("req", req.body)
+  const { cardHolderName, cardNumber } = req.body;
+  if (!cardHolderName || !cardNumber) {
+
+    res.status(400).send({
+      code: 400,
+      message: "please enter proper values."
+    })
+  }
+
+  const AccountData = await Account.create({
+    cardHolderName: cardHolderName,
+    cardNumber: cardNumber
+  });
+  // console.log('AccountData :>> ', AccountData);
+
+  res.status(200).send({
+    code: 200,
+    message: "Successfully register"
+  })
+
+
+
+})
+
+app.get('/api/account', async (req, res) => {
   try {
-      // Fetch expenses data from the database
+    // console.log('AccountData :>> ', AccountData);
+    const cardData = await Account.find({});
+    // console.log('cardData :>> ', cardData);
+    res.status(200).send({
+      code: 200,
+      message: "Successfully register",
+      result : cardData
+
+    })
+  } catch (error) {
+    console.error('Error fetching data from MongoDB:', error);
+    res.status(500).send('Error fetching data');
+  }
+});
+
+app.get('/api/expenses', async (req, res) => {
+  try {
+      // Fetch last 10 expenses data from the database
       const expenses = await Expense.find().sort({ date: -1 }).limit(10);
+
       // Send the fetched expenses data as JSON response
       res.json(expenses);
   } catch (error) {
       res.status(500).json({ message: error.message });
   }
 });
+
 
 // Start the server
 app.listen(PORT, () => {
